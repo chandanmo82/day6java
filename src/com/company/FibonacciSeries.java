690e8765bf0c970eb8817e366ae45565aaa7bf2d
Chandan <chandanmohanty11111>
2021-09-30 07:47:32 +0530

    Day6 Java Practice Problems

---
+++ src/com/company/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int number1 = 0, number2 = 1, number3, i;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Range");
+        int n = sc.nextInt();
+        System.out.print(number1 + " " + number2);
+        for (i = 0; i < n; i++) {
+            number3 = number1 + number2;
+            System.out.print(" " + number3);
+            number1 = number2;
+            number2 = number3;
+        }
+    }
+}
---
+++ src/com/company/PerfectNumber.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Number");
+        int number = sc.nextInt();
+        int i, sum = 0;
+        for (i = 1; i <= (number / 2); i++) {
+            if (number % i == 0) {
+                sum = sum + i;
+            }
+        }
+        if (number == sum) {
+            System.out.println(number + " is a Perfect Number");
+        } else {
+            System.out.println(number + " is not a Perfect Number");
+        }
+    }
+}
